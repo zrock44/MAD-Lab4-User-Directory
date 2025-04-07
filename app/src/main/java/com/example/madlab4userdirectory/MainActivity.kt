7123495a87e60ad18049c4a4a62367ae09061471
zrock44 <zrock44@tutanota.com>
2025-04-06 21:35:14 -0400

    Created a general layout for the UI.

--- app/src/main/java/com/example/madlab4userdirectory/MainActivity.kt
+++ app/src/main/java/com/example/madlab4userdirectory/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.madlab4userdirectory.data.User
 import com.example.madlab4userdirectory.ui.theme.MADLab4UserDirectoryTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +30,56 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MADLab4UserDirectoryTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DirectoryApp()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DirectoryApp(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text("User Directory")
+                }
+            )
+        }
+    ) { innerPadding ->
+        DirectoryContent(modifier = modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun DirectoryContent(modifier: Modifier = Modifier) {
+    UserList(modifier = modifier)
+}
+
+@Composable
+fun UserList(modifier: Modifier = Modifier) {
+    UserCard(User(), modifier = modifier)
+}
+
+@Composable
+fun UserCard(user: User, modifier: Modifier = Modifier) {
+    Card(modifier = modifier.fillMaxWidth()) {
+        Row(modifier = Modifier.padding(8.dp)) {
+            Text(user.image, Modifier.align(Alignment.CenterVertically))
+            Spacer(Modifier.size(30.dp))
+            Column() {
+                Text("${user.title} ${user.firstName} ${user.lastName}")
+                Text(user.email)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DirectoryPreview() {
     MADLab4UserDirectoryTheme {
-        Greeting("Android")
+        DirectoryApp()
     }
 }
